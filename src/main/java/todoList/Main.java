ada20e23ac3a808629044149e3e0dc705f3f4be1
JialiWu <jiali.wu9573@gmail.com>
2022-05-07 01:34:40 +0200

    Editing the Main

--- src/main/java/todoList/Main.java
+++ src/main/java/todoList/Main.java
@@ -3,96 +3,58 @@ package todoList;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import static java.time.temporal.TemporalAdjusters.nextOrSame;
 
 import todoList.tasks.Task;
-import todoList.tasks.TaskFilter;
 import todoList.tasks.TaskList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// create a new task
-		Task newTask = new Task("Read a book", LocalDateTime.now());
-		//System.out.println(newTask.getName());
-		// set new name
-		newTask.setName("do excersice");
-		//System.out.println(newTask.getName());
-		
-		// create a new list of tasks
-		TaskList list = new TaskList();
-		// add three new task
-		list.addNewTask("laundry", LocalDateTime.of(2022, 05, 01, 14, 33, 48));
-		list.addNewTask("read", LocalDateTime.of(2022, 04, 01, 14, 33, 48));
-		list.addNewTask("swim", LocalDateTime.now());
-		list.addNewTask("shower", LocalDateTime.now());
-		// print all tasks in this list
-		for(Task task: list.getList()) {
-			System.out.println(task);
-		}
-		
-		// change the name of one task
-		list.updateName("swim", "read");
-		// print all tasks
-		for(Task task: list.getList()) {
-			System.out.println(task);
-		}
-		
-		// update the finish time of one task and its status
-		list.updateDeadline("laundry", LocalDateTime.now());
-		list.updateStatus("shower", true);
-		// print all tasks
-		for(Task task: list.getList()) {
-			System.out.println(task);
-		}
-		
-		LocalDateTime time = LocalDateTime.now();
-		System.out.println(time.toLocalDate());
-		
-		try {
-			for(Task task: list.getList()) {
-				System.out.println(task.deadlineIsBetween(time.toLocalDate(), time.toLocalDate()));
-			}
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		}
-		
-		TaskFilter taskFilter = new TaskFilter();
-		// print all today list
-		for(Task task: taskFilter.getToday(list)) {
-			System.out.println("today : " + task);
-		}
-		
-		// delete the task of shower
-		list.deleteTask("shower");
-		// print all tasks
-		for(Task task: list.getList()) {
-			System.out.println(task);
-		}
-		
-		// print all today list
-		try {
-			for(Task task: taskFilter.getToday(list)) {
-				System.out.println("today : " + task);
-			}
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		}
-		
-		// print all today list
-		try {
-			for(Task task: taskFilter.getThisWeek(list)) {
-				System.out.println("week : " + task);
-			}
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		}
-		
-		
-
 		LocalDate a = LocalDate.now();
 		System.out.println(a.with(nextOrSame(DayOfWeek.SUNDAY)));
 		
+		TaskList taskList = new TaskList();
+		Task task = new Task("laundry", LocalDateTime.of(2022, 04, 28, 10, 33, 20));
+		
+		taskList.addNewTask(task);
+
+		List<Task> List = new ArrayList<Task>();
+		List.add(task);
+		//List.add(new Task("research", LocalDateTime.of(2022, 05, 01, 14, 33, 48)));
+		//Task task = new Task("swim", LocalDateTime.of(2022, 05, 05, 14, 33, 48));
+		//task.setDeadline(LocalDateTime.of(2022, 05, 06, 14, 33, 48));
+		//List.add(task);
+		//List.add(new Task("shower", LocalDateTime.of(2022, 05, 8, 14, 33, 48)));
+		
+		//boolean isEqual = Objects.equals(taskList.getList(), List);
+		System.out.println(Objects.equals(taskList.getList(), List));
+		
+		for(Task t: taskList.getList()) {
+			System.out.println(t.toString());
+		}
+		for(Task t2: List) {
+			System.out.println(t2.toString());
+		}
+	    //List<ObjTwo> added = list2.stream().filter(o1 -> list.stream().noneMatch(o2 -> o2.getId().equals(o1.getId())))
+	    //         .collect(Collectors.toList());
+
+	    //System.out.print("removed ");
+	    //added.forEach(System.out::println);
+		
+
+		List<Task> difference2 = taskList.getList().stream()
+			    .filter(tsk -> !List.contains(tsk))//tsk.toString() == task.toString())
+			    .collect(Collectors.toList());
+		System.out.print(difference2.isEmpty());
+		
+		System.out.println(java.util.Objects.equals(task, taskList.getList()));
+		System.out.println(List.containsAll(taskList.getList()));
 	}
 
 }
